4c001ad9eaa1a6d6c1531c8ba25b942cf7eda364
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2024-05-21 20:18:30 -0300

    relembrando codigos de comando git e biblioteca math do JavaScript

---
+++ javaScript/math.js
@@ -0,0 +1,4 @@
+console.log(Math.PI) // Mostra o valor de PI
+console.log(Math.pow(2,3)) //Permite usar potenciação
+console.log(Math.ceil(23.6)) //Arredonda o valor pra cima
+console.log(Math.floor(23.6)) //Arredonda o valor para baixo
